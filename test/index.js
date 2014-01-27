8c84f306a17fc0aad61d60b335ca0d8b11a64dbf
Alexey Simonenko <dwarfman@gmail.com>
2014-01-27 23:27:12 +0400

    Fix jshint errors

--- test/index.js
+++ test/index.js
@@ -9,19 +9,21 @@ var oid = '4e4e1638c85e808431000003';
 describe('Easymongo with co', function() {
   it('should works with empty returns', function(done) {
     co(function *() {
+      var result;
+
       yield users.remove();
 
-      var result = yield users.remove();
+      result = yield users.remove();
       result.should.be.false;
 
-      var result = yield users.removeById(oid);
+      result = yield users.removeById(oid);
       result.should.be.false;
 
-      var result = yield users.find({company: 'Yamb'});
+      result = yield users.find({company: 'Yamb'});
       result.should.be.an.instanceof(Array);
       result.should.have.length(0);
 
-      var result = yield users.findById(oid);
+      result = yield users.findById(oid);
       result.should.be.false;
 
       var count = yield users.count();
@@ -31,6 +33,8 @@ describe('Easymongo with co', function() {
 
   it('should works', function(done) {
     co(function *() {
+      var result, count;
+
       yield users.remove();
 
       yield [
@@ -41,7 +45,7 @@ describe('Easymongo with co', function() {
         users.save({title: 'Five', rep: 'meritt/easymongo'})
       ];
 
-      var count = yield users.count();
+      count = yield users.count();
       count.should.be.eql(5);
 
       var documents = yield users.find({rep: 'yamb/yamb'});
@@ -49,17 +53,17 @@ describe('Easymongo with co', function() {
       documents.should.have.length(3);
       documents[0].should.have.property('_id');
 
-      var result = yield users.removeById(documents[2]._id);
+      result = yield users.removeById(documents[2]._id);
       result.should.be.true;
 
-      var result = yield users.findById(documents[1]._id);
+      result = yield users.findById(documents[1]._id);
       result.should.be.an.instanceof(Object);
       result.should.have.property('_id');
 
-      var result = yield users.remove({rep: 'yamb/yamb'});
+      result = yield users.remove({rep: 'yamb/yamb'});
       result.should.be.true;
 
-      var result = yield users.find({rep: 'meritt/easymongo'}, {limit: 1});
+      result = yield users.find({rep: 'meritt/easymongo'}, {limit: 1});
       result.should.be.an.instanceof(Array);
       result.should.have.length(1);
 
@@ -69,7 +73,7 @@ describe('Easymongo with co', function() {
       doc.should.have.property('_id');
       doc.title.should.be.eql('Six');
 
-      var count = yield users.count();
+      count = yield users.count();
       count.should.be.eql(2);
     })(done);
   });
